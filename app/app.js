c03bc8fc29fbd125bfec26440b1766c7abaf3057
Ulquiorrae <hariskrkalic@gmail.com>
2015-06-17 19:17:48 +0200

    initial

---
+++ app/app.js
@@ -0,0 +1,24 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+var app = angular.module('myApp', [
+  'ngRoute',
+  'myApp.view1',
+  'myApp.view2',
+  'myApp.version'
+]).
+config(['$routeProvider', function($routeProvider) {
+  $routeProvider.otherwise({redirectTo: '/view1'});
+}]);
+
+app.controller('TabController', function(){
+    this.tab = 1;
+
+    this.setTab = function(newValue){
+      this.tab = newValue;
+    };
+
+    this.isSet = function(tabName){
+      return this.tab === tabName;
+    };
+  });
